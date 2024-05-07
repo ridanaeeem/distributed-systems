eaf62c792ca7784284501c7f11bd3ee0e4c53bd6
ridanaeeem <ridan@bu.edu>
2024-05-07 17:18:17 -0400

    add comments for channels

--- ws/channel_aggregator.go
+++ ws/channel_aggregator.go
@@ -19,35 +19,43 @@ func channelAggregator(
 	out chan WeatherReport,
 	quit chan struct{},
 ) {
-	// Your code here.
+	// for each batch
 	for batch := 0; ; batch++ {
+		// find current time and when we should send report and move on to next batch
 		cur := time.Now()
 		end := cur.Add(time.Millisecond * time.Duration(averagePeriod * 1000))
-		
+		// channel for goroutines collecting weather data from k stations to send info back to this func
 		ch := make(chan WeatherReport)
 		
+		// for each of the k weather stations, goroutine to get weatherdata and send it back via channel
 		for index := 0; index < k; index++ {
 			go func (i int, ch chan WeatherReport, batch int) {
 				ch <- getWeatherData(i, batch)
 			}(index, ch, batch)
 		}
 
+		// running temp
 		temp := 0.0
+		// count for average
 		reportsSeen := 0
 
 		for {
 			select {
+				// if told to quit, terminate
 				case <- quit:
 					return;
 				default:
 			} 
+			// if it is not time to report yet
 			if time.Now().Before(end){
 				select {
+					// wait for data in channel and use it to compute average
 					case d := <- ch:
 						temp += d.Value
 						reportsSeen ++
 					default:
 				} 
+			// if it is time to report, send report to out channel
 			} else {
 				out <- WeatherReport{temp/float64(reportsSeen), -1, batch}
 				break
