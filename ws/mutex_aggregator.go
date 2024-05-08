6f630b1bb9d8e5e686f63a4790ee679349dbb8fa
ridanaeeem <ridan@bu.edu>
2024-05-08 00:44:18 -0400

    mutex works

--- ws/mutex_aggregator.go
+++ ws/mutex_aggregator.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"sync"
+	"time"
+	// "fmt"
+)
+
 // Mutex-based aggregator that reports the global average temperature periodically
 //
 // Report the averagage temperature across all `k` weatherstations every `averagePeriod`
@@ -14,47 +20,46 @@ func mutexAggregator(
 	out chan WeatherReport,
 	quit chan struct{},
 ) {
-	// Your code here.
-	// batch := 0
-	// cur := time.Now()
-	// end := cur.Add(time.Second * time.Duration(averagePeriod))
+	// mutex so only one goroutine can change temp at a time
+	mutex := sync.Mutex{}
+	// for each batch
+	for batch := 0; ; batch++ {
+		// find current time and when we should send report and move on to next batch
+		cur := time.Now()
+		end := cur.Add(time.Millisecond * time.Duration(averagePeriod * 1000))
+		
+		// running temp
+		temp := 0.0
+		// count for average
+		reportsSeen := 0
+		
+		// for each of the k weather stations, goroutine to get weatherdata and send it back via channel
+		for index := 0; index < k; index++ {
+			go func (i int, batch int) {
+				// once we have data for this batch
+				d := getWeatherData(i, batch)
+				// we can lock the mutex and make adjustments
+				mutex.Lock()
+				temp += d.Value
+				reportsSeen++
+				// after updating running temp and # of reports, unlock mutex 
+				mutex.Unlock()
+			}(index, batch)
+		}
 
-	// for {
-	// 	temp := 0.0
-	// 	select {
-	// 		case message := <- quit:
-	// 			fmt.Println("quit message ", message)
-	// 			close(out)
-	// 		default:
-	// 			if time.Now().Before(end) {
-	// 				for index := 0; index < k; index++ {
-	// 					go func (int) {
-	// 						d := getWeatherData(index, batch)
-	// 						temp += d.Value
-	// 						fmt.Println(d)
-	// 					}(index)
-	// 				}
-	// 			} else {
-	// 				out <- WeatherReport{temp/float64(k), -1, batch}
-	// 				batch ++
-	// 			}
 
-
-
-	// 		// 	for index := 0; index < k; index++ {
-	// 		// 		go func (int) {
-	// 		// 		}(index)
-	// 		// 		if time.Now().Before(end) {
-	// 		// 			d := getWeatherData(index, batch)
-	// 		// 			temp += d.Value
-	// 		// 			fmt.Println(d)
-	// 		// 			// fmt.Println("BEFORE", averagePeriod)
-	// 		// 		} else {
-	// 		// 			// fmt.Println("AFTER", averagePeriod)
-	// 		// 			// out <- WeatherReport{temp/float64(k), index, batch}
-	// 		// 			batch ++
-	// 		// 		}
-	// 		// }
-	// 	}
-	// }
+		for {
+			select {
+				// if told to quit, terminate
+				case <- quit:
+					return;
+				default:
+			} 
+			// if it is time to report 
+			if ! time.Now().Before(end){
+				out <- WeatherReport{temp/float64(reportsSeen), -1, batch}
+				break
+			}
+		}
+	}
 }
